2c6746b9ba7ab755e9d49aad2b90c03647856e10
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-30 10:53:53 -0500

    user resource works

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
 from flask import Flask
 
+from resources.users import users
+
 import models
+
+from flask_cors import CORS
+
+from flask_login import LoginManager
+
+
 
 DEBUG=True
 PORT=8000
 
 app = Flask(__name__)
+
+
+### LoginManager Configuration ###
+
+# SECRET/KEY FOR SESSIONS
+app.secret_key = 'Milo is the best puppy in the universe! This is not a secret.'
+# INSTANTIATE LoginManager
+login_manager = LoginManager()
+# CONNECT APP WITH THE LOGIN MANAGER
+login_manager.init_app(app)
+
+
+
+### CORS -- CROSS ORIGIN RESOURCE SHARING ###
+CORS(users, origins=['http://localhost:3000'], supports_credentials=True)
+
+
+### "CONTROLLERS" ###
+app.register_blueprint(users, url_prefix='/api/v1/users')
 
 
 
---
+++ resources/users.py
@@ -0,0 +1,9 @@
+import models
+
+from flask import Blueprint
+
+users = Blueprint('users', 'users')
+
+@users.route('/', methods=['GET'])
+def test_user():
+	return 'user resource works'
