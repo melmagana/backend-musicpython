aa7613eabecf38ee2ad8a4158658b9df9fd9aee3
Melissa Magaña <melissamagana@melissas-mbp.attlocal.net>
2020-04-30 11:32:09 -0500

    user gets created and checks if they already exist

--- resources/users.py
+++ resources/users.py
@@ -1,6 +1,7 @@
 import models
 
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
+from flask_bcrypt import generate_password_hash
 
 users = Blueprint('users', 'users')
 
@@ -11,5 +12,39 @@ def test_user():
 
 @users.route('/register', methods=['POST'])
 def register():
-	print(request.get_json())
+	
+	payload = request.get_json()
+	payload['name'] = payload['name']
+	payload['username'] = payload['username'].lower()
+	payload['email'] = payload['email'].lower()
+	print(payload)
+
+
+	# LOGIC TO SEE IF USER EXISTS
+	try:
+		models.User.get(models.User.email == payload['email'])
+
+		return jsonify(
+			data={},
+			message=f"A user with the email {payload['email']} already exists",
+			status=401
+		), 401
+
+
+	except models.DoesNotExist:
+
+		# scramble password with bcrypt
+		pw_hash = generate_password_hash(payload['password'])
+
+
+		# CREATE USER
+		create_user = models.User.create(
+			name=payload['name'],
+			username=payload['username'],
+			email=payload['email'],
+			password=pw_hash
+		)
+
+		print(create_user)
+
 	return 'check terminal'
